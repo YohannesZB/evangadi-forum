c3fb2aa17edd5c0db5acd808425c20de47d7e8d0
Kedus1922 <152933553+Kedus1922@users.noreply.github.com>
2024-03-25 22:34:27 -0400

    Add api deployed endpoint

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosBase = axios.create({
-      baseURL: 'http://localhost:8000/api'
+      baseURL: 'https://evangadi-forum-vwj2.onrender.com/api'
 })
 
 export default axiosBase
