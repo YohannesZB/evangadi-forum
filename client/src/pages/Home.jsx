744dbc2668f084887d3dc75b10340997275e79f0
Kedus1922 <152933553+Kedus1922@users.noreply.github.com>
2024-03-26 22:14:58 -0400

    Footer.jsx and footer.css

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,96 +1,10 @@
-// import React, {useContext} from 'react'
-// import Header from '../components/Header/Header'
-// import { useNavigate, Link } from 'react-router-dom';
-// import "../../node_modules/bootstrap/dist/css/bootstrap.css"
-// import { FaAngleRight } from 'react-icons/fa6';
-// import { PiUserCircleDuotone } from 'react-icons/pi';
-// import { AppState } from '../App';
-
-// const Home = () => {
-//   const { user, setUser, question, setQuestion } = useContext(AppState);
-//   console.log(question);
-//   console.log(user);
-
-
-//     const navigate = useNavigate();
-//   // const handleAnswerClick = () => {
-//   //   navigate("/answer"); 
-//   // };
-
-//   const handleAskQuestionClick = () => {
-//     navigate("/questions"); 
-//   };
-//   return (
-//     <>
-//     <Header />
-
-//     <section className='bg-body-tertiary'>
-    
-//     <hr />
-//     <div className='d-flex justify-content-around pt-5 bg-body-tertiary'>
-
-//        <a
-//             href="#"
-//             onClick={handleAskQuestionClick}
-//             className="btn btn-primary action_btn px-5"
-//           >
-           
-//             Ask Question
-//           </a>
-//       <p className='fw-semibold'> <span className='text-warning'>Welcome,</span>{user.username}</p>
-//     </div>
-    
-
-//     <div className='container mt-5'>
-
-//     <h2>Questions</h2>
-//     {question.allquestion && question.allquestion.map((item, index) => (
-//       <Link
-//       className="text-decoration-none text-black"
-//       key={index}
-//       to={`/answer?title=${encodeURIComponent(item.title)}&description=${encodeURIComponent(item.description)}&questionid=${encodeURIComponent(item.questionid)}`}
-//     >
-    
-
-//       <hr />
-//       <div className="d-flex justify-content-between align-items-center ">
-//         <div className="d-flex flex-column align-items-center gap-3">
-//           {/* user */}
-//           <div className="user-icon">
-//             <PiUserCircleDuotone />
-//           </div>
-//           <div>{user.username}</div>
-//         </div>
-//         <div>
-//           {/* question */}
-//           <p>{item.title}</p>
-//         </div>
-//         <div>
-//           {/* arrow */}
-//           <FaAngleRight />
-//         </div>
-//       </div>
-//     </Link>
-//     ))}
-   
-
-//     </div>
-//     </section>
-//     </>
-//   )
-// }
-
-// export default Home
-
-
-
-
 import React, { useContext } from "react";
 import Header from "../components/Header/Header";
 import { useNavigate, Link } from "react-router-dom";
 import { PiUserCircleDuotone } from "react-icons/pi";
 import { FaAngleRight } from "react-icons/fa6";
 import { AppState } from "../App";
+import Footer from "../components/Footer/Footer";
 
 const Home = () => {
   const { user, question } = useContext(AppState);
@@ -161,6 +75,7 @@ const Home = () => {
             ))}
         </div>
       </section>
+      <Footer />
     </>
   );
 };
